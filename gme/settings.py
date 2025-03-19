e462faf97cd7662238ada4b1b90e5bf82173cf05
bl175 <bl.17527@khcc.jo>
2025-03-19 14:04:50 +0300

    email system implemented

--- gme/settings.py
+++ gme/settings.py
@@ -137,12 +137,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'gme@khcc.jo')
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.office365.com')  # Default to Office 365
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'mail.khcc.jo')  # Default to Office 365
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '25'))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'False').lower() == 'true'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'gme@khcc.jo')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
 # In development, use console backend
 if DEBUG:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    # Comment out the next line temporarily to test real email sending
+    # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    pass

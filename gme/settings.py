96fd96b884b3e239463ad91bcdf6ce3f70cd6149
Mais <ma.17464@khcc.jo>
2025-03-26 09:46:36 +0300

    update

--- gme/settings.py
+++ gme/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4##o^-dy&3z3@a_(dh*@2x$54f^(l3lwnpnwpjgeu_3&$4gka(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
